70a80ab94633751ecad6178b2b047cfaa861e3b8
Pragati246 <pragatitomar246@gmail.com>
2024-02-14 20:42:48 +0530

    SignIn/SignUp form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,42 @@
-import Header from "./Header"
+import Header from "./Header";
+import { useState } from "react"
 const Login = () => {
+    const [isSignInForm , setIsSignInForm ] = useState(true);
+
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm);
+    }
+
+    
     return (
         <div>
             <Header/>
-            <div>
-            <img
+            <div className="absolute">
+            <img 
                 src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"
                 alt="logo"
             />
             </div>
-            <form>
-                <input type="text" placeholder="Email Address" className="p-2 m-2"/>
-                <input type="text" placeholder="Password" className="p-2 m-2"/>
-                <button className="p-4 m-4">Sign In</button>
+            <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+                <h1 className="font-bold text-3xl py-4 w-full">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </h1>
+                {!isSignInForm && (
+                <input type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700"/>)}
+                <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+                <input type="text" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
+                <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </button>
+                <h3>Forgot Password?</h3>
+                <p className="py-4" onClick={toggleSignInForm}>
+                    {
+                        isSignInForm
+                        ? "New to netflix? Sign Up Now"
+                        : "Already registered ? Sign In Now"
+                    }
+                </p>
+
             </form>
         </div>
     )
